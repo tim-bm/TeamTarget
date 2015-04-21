7bbbd3c2a112c9b20925affce14a7a95549dcdd3
tim <tim.cbt@gmail.com>
2015-04-21 11:18:44 +0800

    UI: Add new-project activity and listener for listView

--- app/src/main/java/bm/com/graduationproject/teamtarget/MainActivity.java
+++ app/src/main/java/bm/com/graduationproject/teamtarget/MainActivity.java
@@ -2,6 +2,7 @@ package bm.com.graduationproject.teamtarget;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -108,7 +109,6 @@ public class MainActivity extends FragmentActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-
         switch(id){
 
             case(R.id.action_new):
@@ -132,6 +132,7 @@ public class MainActivity extends FragmentActivity {
 
         PopupMenu popupMenu=new PopupMenu(this,menuItemView);
 
+        popupMenu.setOnMenuItemClickListener(new innerMenuItemListener());
         popupMenu.getMenuInflater()
                 .inflate(R.menu.main_activity_actions_new_item, popupMenu.getMenu());
 
@@ -155,10 +156,43 @@ public class MainActivity extends FragmentActivity {
             e.printStackTrace();
         }
 
-
-
         popupMenu.show();
     }
 
 
+    // inner class for popup menu item when clicked
+    class innerMenuItemListener implements PopupMenu.OnMenuItemClickListener{
+
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()){
+
+                case R.id.new_project:
+
+                    newProject();
+
+                    return true;
+
+                case R.id.new_task:
+
+                    return  true;
+
+                case R.id.new_schedule:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+
+
+    private void newProject(){
+
+        startActivity(new Intent(this,NewProjectActivity.class));
+       // animation
+      overridePendingTransition(R.anim.push_up_in,R.anim.stand_still);
+       // overridePendingTransition(android.R.anim.slide_out_right,android.R.anim.slide_in_left);
+    }
 }
--- app/src/main/java/bm/com/graduationproject/teamtarget/NewProjectActivity.java
+++ app/src/main/java/bm/com/graduationproject/teamtarget/NewProjectActivity.java
@@ -1,17 +1,42 @@
 package bm.com.graduationproject.teamtarget;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class NewProjectActivity extends Activity {
 
+    private ArrayList<HashMap<String,Object>> newItemList;
+    private SimpleAdapter newItemListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+            super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_project);
+        getActionBar().setIcon(
+                new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+
+
+        ListView newItemListView=(ListView)findViewById(R.id.new_list_item_selected);
+
+        String[] strKeys={"icon","hint","selected"};
+        int[] ids={R.id.array_new_icon,R.id.array_hint,R.id.array_selected};
+        newItemListAdapter=new SimpleAdapter(this,getList(),R.layout.array_new_item,strKeys,ids);
+
+        newItemListView.setAdapter(newItemListAdapter);
+
+
     }
 
 
@@ -32,8 +57,40 @@ public class NewProjectActivity extends Activity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+           //animation
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+
+            this.finish();
+            overridePendingTransition(R.anim.stand_still, R.anim.pull_down_out);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private List<HashMap<String,Object>> getList(){
+
+        ArrayList<HashMap<String,Object>> list=new ArrayList<HashMap<String, Object>>(2);
+        HashMap<String,Object> map=null;
+
+        for(int i=0;i<2;i++){
+            map = new HashMap<String, Object>();
+            map.put("icon",R.drawable.icon_bruch);
+            map.put("hint","所有者");
+            map.put("selected","公开项目");
+
+            list.add(map);
+        }
+
+        return list;
     }
 }
